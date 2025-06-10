c7e41c61d5313a1a64020159a5c7342d5f9cf97c
PawelSalnik <pawel.salnik@gmail.com>
2025-06-10 19:22:14 +0200

    Dodano DefaultFileNames.cs

---
+++ bibModelSalnik/Model/DefaultFileNames.cs
@@ -0,0 +1,9 @@
+﻿namespace bibModelSalnik.Model
+{
+    public static class DefaultFileNames
+    {
+        public const string plikAutorzy = "autorzy_TwojeNazwisko.xml";
+        public const string plikWydawnictwa = "wydawnictwa_TwojeNazwisko.xml";
+        public const string plikKsiazki = "ksiazki_TwojeNazwisko.xml";
+    }
+}
