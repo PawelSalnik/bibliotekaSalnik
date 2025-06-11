97d38989c584ea7ee5c998879f0d928b79aa6761
PawelSalnik <pawel.salnik@gmail.com>
2025-06-11 17:49:21 +0200

    dodano KsiazkiKsiazkaExt.cs

---
+++ bibModelSalnik/Model/KsiazkiKsiazkaExt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using bibModelSalnik.Model;
+
+namespace bibModeSalnik.Model
+{
+    public class KsiazkiKsiazkaExt : KsiazkiKsiazka
+    {
+        public string nazwiskoImie { get; set; }  // np. "Mickiewicz Adam"
+        public string nazwaWydawnictwa { get; set; }
+    }
+}
+
